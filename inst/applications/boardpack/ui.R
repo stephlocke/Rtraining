af1bfc02a4bc0104c0183d024dbbfb7690a27bf8
Steph Locke <stephanie.g.locke@gmail.com>
2015-06-30 16:21:57 +0100

    Switched to big text boxes

--- inst/applications/boardpack/ui.R
+++ inst/applications/boardpack/ui.R
@@ -2,12 +2,15 @@ library(shiny)
 
 shinyUI(
   bootstrapPage(
-    textInput("TableD1","TableD1","blah"),
-    textInput("ChartD1","ChartD1","meh"),
-    textInput("TableD2","TableD2","huh"),
-    textInput("ChartD2","ChartD2","wtf?"),
+    h1("Monthly board pack"),
+    h2("Add commentary here"),
+    HTML('<textarea id="TableD1" rows="3" cols="500">TableD1</textarea><br>')   ,
+    HTML('<textarea id="ChartD1" rows="3" cols="500">ChartD1</textarea><br>')   ,
+    HTML('<textarea id="TableD2" rows="3" cols="500">TableD2</textarea><br>')   ,
+    HTML('<textarea id="ChartD2" rows="3" cols="500">ChartD2</textarea><br>')   ,
+    h2("Download the PDF when you're happy"),
     downloadButton('report'),
-            h2("Board pack preview"),
-            htmlOutput("knitDoc")
-        )
-    )
+    h2("Preview the Board pack"),
+    htmlOutput("knitDoc")
+  )
+)
