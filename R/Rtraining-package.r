4776bf6c733b2fa6de406240ffaba88cf3622bd9
Steph Locke <stephanie.g.locke@gmail.com>
2015-07-27 08:55:05 +0100

    Added RODBC

--- R/Rtraining-package.r
+++ R/Rtraining-package.r
@@ -3,5 +3,5 @@
 #' @name Rtraining
 #' @docType package
 #' @import data.table httr devtools jsonlite Hmisc magrittr dplyr DiagrammeR shiny knitr foreach
-#' @import memisc optiRum readxl readr
+#' @import memisc optiRum readxl readr RODBC
 NULL
