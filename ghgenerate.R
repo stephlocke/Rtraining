cc3594c778c99ffada1460095a6b73d1418516d8
unknown <slocke@slocke.Mango.local>
2015-09-03 12:58:47 +0100

    Tweaked autogen directory str

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,5 +1,9 @@
-slidedecks<-list.files("inst/slidedecks/", pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
-for(f in slidedecks) rmarkdown::render(f, output_format="io_slides",output_dir="inst/doc")
+slidedecks<-list.files(system.file("slidedecks",package = "Rtraining"), 
+                       pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
+for(f in slidedecks) rmarkdown::render(f, output_format="io_slides",
+                                       output_dir="inst/doc")
 
-handouts<-list.files("inst/handouts/", pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
-for(f in handouts) rmarkdown::render(f, output_format="html_documents",output_dir="inst/doc")
+handouts<-list.files(system.file("handouts",package = "Rtraining"),
+                     pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
+for(f in handouts) rmarkdown::render(f, output_format="html_documents",
+                                     output_dir="inst/doc")
