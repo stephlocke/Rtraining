e9ea422bd1e76ae1fcdf1c0d18599e83e8c67ced
Steph Locke <slocke@mango-solutions.com>
2015-11-02 17:14:21 +0000

    Removed some problematic auto-gen files

--- ghgenerate.R
+++ ghgenerate.R
@@ -4,9 +4,11 @@ message(list.files("../Rtraining" ,recursive = TRUE))
 slidedecks<-list.files("../Rtraining/inst/slidedecks/", 
                        pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
 for(f in slidedecks) rmarkdown::render(f, output_format="ioslides_presentation",
-                                       output_dir=".")
+                                       output_dir=".",quiet=TRUE)
 
 handouts<-list.files("../Rtraining/inst/handouts/",
                      pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
+handouts<-handouts[-grep("(javascript|frontmatter)",x=handouts)]
 for(f in handouts) rmarkdown::render(f, output_format="html_document",
-                                     output_dir=".")
+                                     output_dir=".",quiet=TRUE)
+
