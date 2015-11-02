7407cfb25183d1983babae70d12022ed96a1ea77
Steph Locke <slocke@mango-solutions.com>
2015-11-02 18:53:36 +0000

    DiagrammeR fix https://github.com/rich-iannone/DiagrammeR/issues/139

--- ghgenerate.R
+++ ghgenerate.R
@@ -5,10 +5,10 @@ slidedecks<-list.files("../Rtraining/inst/slidedecks/",
                        pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
 for(f in slidedecks) rmarkdown::render(f, output_format="ioslides_presentation",
                                        output_dir=".",quiet=TRUE)
-
+devtools::install_version("DiagrammeR","0.7","https://cran.rstudio.com")
 handouts<-list.files("../Rtraining/inst/handouts/",
                      pattern = "*.Rmd",recursive = TRUE,full.names = TRUE)
 handouts<-handouts[-grep("(javascript|frontmatter|windows)",x=handouts)]
 for(f in handouts) rmarkdown::render(f, output_format="html_document",
-                                     output_dir=".",quiet=TRUE)
+                                     output_dir=".")
 
