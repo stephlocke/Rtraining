a4dfaef76bdeae59dadca05e533e05a9d5e54d9f
Steph Locke <slocke@mango-solutions.com>
2015-11-06 18:34:39 +0000

    Tweaking based on folder structures

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -7,17 +7,12 @@ GH_REPO="@github.com/stephlocke/Rtraining.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-for files in '*.tar.gz'; do
-        tar xfz $files
-done
+R CMD BATCH '../Rtraining/ghgenerate.R'
 
 cd out
 git init
 git config user.name "stephs-travis"
 git config user.email "travis"
-
-R CMD BATCH '../Rtraining/ghgenerate.R'
-
 git add .
 git commit -m "deployed to github pages"
 git push --force --quiet $FULL_REPO master:gh-pages
