8551336f3faee7dc2711f8074f4a61c6713c583d
Steph Locke <slocke@mango-solutions.com>
2015-12-11 07:13:16 +0000

    SQLSat Slovenia

--- R/Rtraining-package.r
+++ R/Rtraining-package.r
@@ -4,4 +4,5 @@
 #' @docType package
 #' @import data.table httr devtools jsonlite Hmisc magrittr dplyr DiagrammeR shiny knitr foreach
 #' @import memisc optiRum readxl readr RODBC testthat knitr rmarkdown leaflet
+#' @import dygraphs plotly
 NULL
