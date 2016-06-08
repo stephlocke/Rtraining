df5df5dc2225d06fb160965b90a7513ebb828dae
Steph Locke <steph.locke@censornet.com>
2016-06-08 05:35:36 +0100

    Added workshops

--- ghgenerate.R
+++ ghgenerate.R
@@ -10,11 +10,19 @@ shinydecks<-list.files(
   "inst/slidedecks/shiny",
   pattern = "*.Rmd",recursive = TRUE,full.names = TRUE
 )
+
+workshops<-list.files(
+  "inst/workshops",
+  pattern = "*.Rmd",recursive = TRUE,full.names = TRUE
+)
   
 slidedecks<-slidedecks[-grep("subDocs|demo",slidedecks)]
+workshops<-workshops[-grep("subDocs|demo",workshops)]
 slidedecks<-setdiff(slidedecks,shinydecks)
+
 for (f in slidedecks) render(f,output_dir = "out",output_format = Rtraining::stephRevealSlideStyle())
 for (f in shinydecks) render(f,output_dir = "out",output_format = Rtraining::stephSlideStyle())
+for (f in workshops) render(f, output_dir = "out")
 
 handouts <- list.files(
   "inst/handouts/",
