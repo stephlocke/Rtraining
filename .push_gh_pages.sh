b15b484c1ae0724f3853d68ba6f9fb988006dd47
Steph Locke <steph@itsalocke.com>
2017-02-15 20:42:45 +0000

    Simplifying deploy code

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+FULL_REPO="https://$GH_TOKEN$@github.com/$TRAVIS_REPO_SLUG.git"
 
-rm -rf out || exit 0;
-mkdir out;
-
-GH_REPO="@github.com/stephlocke/Rtraining.git"
-
-FULL_REPO="https://$GH_TOKEN$GH_REPO"
-
-git clone https://github.com/stephlocke/RTraining.git out --branch gh-pages
+git clone $FULL_REPO out --branch gh-pages
 cd out
 git config user.name "stephs-travis"
-git config user.email "travis"
+git config user.email "steph@travis.ci"
 
 cd ..
 R CMD BATCH '../Rtraining/ghgenerate.R'
